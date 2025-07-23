10c74ef9040097cfe5cb26db3a90605d5696691e
Nouvy <95512507+Nouvy@users.noreply.github.com>
2025-07-23 08:40:57 +0200

    v4

--- src/main/scala/ProduitDAO.scala
+++ src/main/scala/ProduitDAO.scala
@@ -1,15 +1,33 @@
 import java.sql.PreparedStatement
 import java.sql._
 object ProduitDAO {
-  def insert(produit: Produit) = {
+  def insert(produit: Produit): Int = {
       val requete = "INSERT INTO produits " +
         "(nom, description, stock)" +
         "VALUES (?, ?, ?)"
-      val statement = DB.connection.prepareStatement(requete)
+      val statement = DB.connection.prepareStatement(requete, java.sql.Statement.RETURN_GENERATED_KEYS)
       statement.setString(1, produit.nom)
       statement.setString(2, produit.description)
       statement.setInt(3, produit.stock)
       statement.executeUpdate()
+
+      val ids = statement.getGeneratedKeys
+      if (ids.next()) {
+        val idProduit = ids.getInt(1)
+        val requeteLiaison = "INSERT INTO produits_categories" +
+          "(produit_id, categorie_id) " +
+          "VALUES (?, ?)"
+        val statementLiason = DB.connection.prepareStatement(requeteLiaison)
+        for (categorie <- produit.categories) {
+          statementLiason.setInt(1, idProduit)
+          statementLiason.setInt(2, categorie.id)
+          statementLiason.executeUpdate()
+        }
+
+        idProduit
+      } else {
+        throw new Exception("Erreur de récupération de l'id du produit")
+      }
     }
 
   def findAll(): List[Produit] = {
@@ -29,5 +47,5 @@ object ProduitDAO {
       produits = produit :: produits
     }
     produits.reverse
-  }
+    }
   }
